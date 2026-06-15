bb1b85ef91ce8575d284c2c0a8e82137e5a2c336
Mariocoleguilla <mariocoleguilla@hotmail.com>
2026-06-15 13:48:40 +0200

    test: remove obsolete title verification test case from app component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mortgageCalculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mortgageCalculator');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
